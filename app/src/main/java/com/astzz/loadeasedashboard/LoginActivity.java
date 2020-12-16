80097206dafcc01e703ddbc45d8f81e916c797f3
usama khan <hafizahmed420@gmail.com>
2020-12-16 16:30:57 +0500

    nnn

--- app/src/main/java/com/astzz/loadeasedashboard/LoginActivity.java
+++ app/src/main/java/com/astzz/loadeasedashboard/LoginActivity.java
@@ -3,24 +3,65 @@ package com.astzz.loadeasedashboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import com.budiyev.android.circularprogressbar.CircularProgressBar;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     TextView sifn_up;
 
+    BarChart barChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.fragment_dashboard);
+//
 
-        sifn_up  =findViewById(R.id.sign_up);
-        sifn_up.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
-            startActivity(intent);
-        });
+
+        CircularProgressBar progressBar = findViewById(R.id.progress_bar);
+        progressBar.setProgress(30f);
+
+//        sifn_up  =findViewById(R.id.sign_up);
+//        sifn_up.setOnClickListener(view -> {
+//            Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+//            startActivity(intent);
+//        });
+
+        barChart = findViewById(R.id.barChart);
+        ArrayList<BarEntry> visitor= new ArrayList<>();
+        visitor.add(new BarEntry(2014,120));
+        visitor.add(new BarEntry(2015,220));
+        visitor.add(new BarEntry(2016,380));
+        visitor.add(new BarEntry(2017,400));
+        visitor.add(new BarEntry(2018,520));
+        visitor.add(new BarEntry(2019,620));
+
+
+        BarDataSet barDataSet = new BarDataSet(visitor,"visitors") ;
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Bar Chart");
+        barChart.animateY(2000) ;
+
     }
+
 }
--- app/src/main/java/com/astzz/loadeasedashboard/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/astzz/loadeasedashboard/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.astzz.loadeasedashboard.ui.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,23 +14,49 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.astzz.loadeasedashboard.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+
+
+        BarChart barChart;
+
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        barChart = root.findViewById(R.id.barChart);
+        ArrayList<BarEntry> visitor= new ArrayList<>();
+        visitor.add(new BarEntry(2014,420));
+        visitor.add(new BarEntry(2014,420));
+        visitor.add(new BarEntry(2014,420));
+        visitor.add(new BarEntry(2014,420));
+        visitor.add(new BarEntry(2014,420));
+        visitor.add(new BarEntry(2014,420));
+
+
+        BarDataSet barDataSet = new BarDataSet(visitor,"visitors") ;
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Bar Chart");
+        barChart.animateY(2000) ;
+
+
         return root;
     }
 }
